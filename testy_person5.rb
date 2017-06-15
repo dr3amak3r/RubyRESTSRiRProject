08082b8734401e8638b3722a0e2c09991cd3ff0f
Michał Gogól <michal.gogol@yahoo.com>
2017-06-15 19:37:39 +0200

    testy dla osoby 5

--- testy_person5.rb
+++ testy_person5.rb
@@ -1,26 +1,32 @@
+#!/usr/bin/env  ruby
+load 'person5.rb'
+
 require "test/unit"
 
-load person5.rb
 
+# @author Michał
+# test funkcji odpowiedzialnej za bezpieczną kompilację
+#
 class TestSecurity < Test::Unit::TestCase
 
 
+  #Kod który jest bezpieczny dla działania systemu, powinien zwrócić true
   def test_simple_code
-    result = safetyCheck('temporary.rb')
+    result = safetyCheck('./test_case/hello_world.rb')
     puts result
-    assert_equal(false, result)
+    assert_equal(true, result)
   end
 
-
+  #Kod próbujący sformatować dysk, nie powienien się skompilować i zwrócić false
   def test_harmful_code
-    result = safetyCheck('ExternalCode.rb')
+    result = safetyCheck('./test_case/format_system.rb')
     assert_equal(false,result)
     puts result
   end
 
-
+  #Kod wywołujący kolejny podprogram powinien zwrócić false
   def test_harmful_code2
-    result = safetyCheck('ExternalCode.rb')
+    result = safetyCheck('./test_case/Invoke_another.rb')
     puts result
     assert_equal(false,result)
   end
