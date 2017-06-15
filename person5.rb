39ab1be34bb46d3d805d33b94d12d6539624c001
Michał Gogól <michal.gogol@yahoo.com>
2017-06-15 21:26:33 +0200

    Create person5.rb

--- person5.rb
+++ person5.rb
@@ -3,7 +3,8 @@
 # @author Michał
 #
 #Funkcja sprawdza poziom bezpieczeństwa kodu
-#Ustawiając poziom bezpieczeństwa $SAFE=3, modyfikując kopię otrzymanego kodu
+#Ustawiając poziom bezpieczeństwa $SAFE=3,modyfikując kopię otrzymanego kodu
+#który uniemożliwia dostęp i modyfikację do zasobów zewnętrzych hosta
 #Jeżeli w kodzie występują potencjalnie niebezpieczne metody zwróci 'false'
 #
 # @param dir[String] - sciezka do pliku
@@ -20,7 +21,7 @@ def safetyCheck(dir)
       |f| f << '$SAFE = 3; '<< to_check
   }
 
-
+#Wywołanie zmodyfikowanego pliku z kodem
   begin
     eval(IO.read(temp_file))
 
